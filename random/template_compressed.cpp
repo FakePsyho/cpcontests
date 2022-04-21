19ffa4afc19d89e533da1ef6be5e98937bb41725
Psyho <closedeyez@gmail.com>
2022-04-21 18:34:32 +0200

    standard marathon template (uploaded for the stream)

---
+++ random/template_compressed.cpp
@@ -0,0 +1,98 @@
+// Author: Psyho
+// Twitter: https://twitter.com/fakepsyho
+ 
+#include <bits/stdc++.h>
+#include <sys/time.h>
+ 
+using namespace std;
+ 
+#define INLINE   inline __attribute__ ((always_inline))
+#define NOINLINE __attribute__ ((noinline))
+
+#define byte        unsigned char
+#define FOR(i,a,b)  for(int i=(a);i<(b);++i)
+#define REP(i,a)    FOR(i,0,a)
+#define ZERO(m)     memset(m,0,sizeof(m))
+#define MINUS(m)    memset(m,-1,sizeof(m))
+#define ALL(x)      x.begin(),x.end()
+#define PB          push_back
+#define S           size()
+#define LL          long long
+#define ULL         unsigned long long
+#define LD          long double
+#define MP          make_pair
+#define X           first
+#define Y           second
+#define VC          vector
+#define PII         pair<int, int>
+#define PDD         pair<double, double>
+#define PIII        pair<PII, int>
+#define VB          VC<byte>
+#define VVB         VC<VB>
+#define VI          VC<int>
+#define VVI         VC<VI>
+#define VVVI        VC<VVI>
+#define VPII        VC<PII>
+#define VVPII       VC<VPII>
+#define VD          VC<double>
+#define VVD         VC<VD>
+#define VVVD        VC<VVD>
+#define VVVVD       VC<VVVD>
+#define VF          VC<float>
+#define VVF         VC<VF>
+#define VVVF        VC<VVF>
+#define VS          VC<string>
+#define VVS         VC<VS>
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, pair<A,B> p) {os << "(" << p.X << "," << p.Y << ")"; return os;}
+template<typename A, typename B, typename C> ostream& operator<<(ostream &os, tuple<A,B,C> p) {os << "(" << get<0>(p) << "," << get<1>(p) << "," << get<2>(p) << ")"; return os;}
+template<typename T> ostream& operator<<(ostream &os, VC<T> v) {os << "{"; REP(i, v.S) {if (i) os << ", "; os << v[i];} os << "}"; return os;}
+template<typename T> ostream& operator<<(ostream &os, set<T> s) {VS vs(ALL(s)); return os << vs;}
+template<typename T> string i2s(T x) {ostringstream o; o << x; return o.str();}
+VS splt(string s, char c = ' ') {VS all; int p = 0, np; while (np = s.find(c, p), np >= 0) {if (np != p) all.PB(s.substr(p, np - p)); p = np + 1;} if (p < s.S) all.PB(s.substr(p)); return all;}
+
+#define ARGS_SIZE_(a1,a2,a3,a4,a5,a6,a7,a8,a9,size,...) size
+#define ARGS_SIZE(...) ARGS_SIZE_(__VA_ARGS__,9,8,7,6,5,4,3,2,1)
+
+#define DB_1(x) #x << "=" << (x) <<
+#define DB_2(x, ...) #x << "=" << (x) << ", " << DB_1(__VA_ARGS__)
+#define DB_3(x, ...) #x << "=" << (x) << ", " << DB_2(__VA_ARGS__)
+#define DB_4(x, ...) #x << "=" << (x) << ", " << DB_3(__VA_ARGS__)
+#define DB_5(x, ...) #x << "=" << (x) << ", " << DB_4(__VA_ARGS__)
+#define DB_6(x, ...) #x << "=" << (x) << ", " << DB_5(__VA_ARGS__)
+#define DB_7(x, ...) #x << "=" << (x) << ", " << DB_6(__VA_ARGS__)
+#define DB_8(x, ...) #x << "=" << (x) << ", " << DB_7(__VA_ARGS__)
+#define DB_9(x, ...) #x << "=" << (x) << ", " << DB_8(__VA_ARGS__)
+#define DB__(size, ...) DB_##size(__VA_ARGS__)
+#define DB_(size, ...) DB__(size, __VA_ARGS__)
+#define DB(...) do {cout << DB_(ARGS_SIZE(__VA_ARGS__), __VA_ARGS__) endl;} while (0)
+ 
+double get_time() {timeval tv; gettimeofday(&tv, NULL); return tv.tv_sec + tv.tv_usec * 1e-6;}
+ 
+struct RNG {
+    unsigned int MT[624];
+    int index;
+    RNG(int seed = 1) {init(seed);}
+    void init(int seed = 1) {MT[0] = seed; FOR(i, 1, 624) MT[i] = (1812433253UL * (MT[i-1] ^ (MT[i-1] >> 30)) + i); index = 0; }
+    void generate() {
+        const unsigned int MULT[] = {0, 2567483615UL};
+        REP(i, 227) {unsigned int y = (MT[i] & 0x8000000UL) + (MT[i+1] & 0x7FFFFFFFUL); MT[i] = MT[i+397] ^ (y >> 1); MT[i] ^= MULT[y&1]; }
+        FOR(i, 227, 623) {unsigned int y = (MT[i] & 0x8000000UL) + (MT[i+1] & 0x7FFFFFFFUL); MT[i] = MT[i-227] ^ (y >> 1); MT[i] ^= MULT[y&1]; }
+        unsigned int y = (MT[623] & 0x8000000UL) + (MT[0] & 0x7FFFFFFFUL); MT[623] = MT[623-227] ^ (y >> 1); MT[623] ^= MULT[y&1];
+    }
+    unsigned int rand() { if (index == 0) generate(); unsigned int y = MT[index]; y ^= y >> 11; y ^= y << 7  & 2636928640UL; y ^= y << 15 & 4022730752UL; y ^= y >> 18; index = index == 623 ? 0 : index + 1; return y;}
+    INLINE int next() {return rand(); }
+    INLINE int next(int x) {return rand() % x; }
+    INLINE int next(int a, int b) {return a + (rand() % (b - a)); }
+    INLINE double next_double() {return (rand() + 0.5) * (1.0 / 4294967296.0); }
+    INLINE double next_double(double a, double b) {return a + next_double() * (b - a); }
+};
+ 
+static RNG rng;
+
+double start_time = get_time();
+
+int main(int argc, char **argv) {
+	
+	return 0;
+}
